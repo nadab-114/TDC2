401f07f6f904a50ffc5bd0918944b39c4aee5177
nadab-114 <josenadabgithub@gmail.com>
2024-11-14 07:10:49 +0800

    2nd

--- resources/views/assessment/index.blade.php
+++ resources/views/assessment/index.blade.php
@@ -7,7 +7,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
+<body class="flex flex-wrap">
+    
+    <iframe
+      id="JotFormIFrame-243174886545468"
+      title="Quiz form with a calculated number of correct answers"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen"
+      src="https://form.jotform.com/243174886545468"
+      frameborder="0"
+      style="min-width:100%;max-width:100%;height:539px;border:none;"
+      scrolling="no">
+    </iframe>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-243174886545468']", "https://form.jotform.com/")</script>
     
 </body>
 </html>
